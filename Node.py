c78776d89e97e4333157d8a353dac36cfd6c8312
Cornelius-Sivertsen <150359082+Cornelius-Sivertsen@users.noreply.github.com>
2026-01-20 15:22:56 +0100

    Continue improving documentation

--- Node.py
+++ Node.py
@@ -1,3 +1,4 @@
+# Implements the nodes of the B&B tree
 from constants import n, S, D
 from Cij import C_ij
 
@@ -9,20 +10,27 @@ class SolutionNode:
         self.cons = cons
         self.parent = parent
 
+    # Classic B&B bound() function
     def bound(self):
         return self.cost + (n-self.i)*S[0]
 
+    # Returns true if node is a terminal node
     def solution(self):
         return (self.i == n)
 
+    # Classic B&B branch() function
     def branch(self):
         nodes = list(range(len(S)))
 
         for l in range(len(S)):
             nodes[l] = SolutionNode(self.i + 1, l, self.cost + S[l], 0, self)
+            # Note: cons is set to 0 here because we want to calculate the true cons as
+            # rarely as possible. It is therefore conditionally calculated and set later
+            # using the .constraint() member function
 
         return nodes
 
+    # Calculates the cons field of a node.
     def constraint(self):
         return self.parent.cons + C_ij.get_Cij(self.i,self.j) / D[self.i]
 
